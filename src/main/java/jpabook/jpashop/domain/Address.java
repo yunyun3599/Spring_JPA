17d4792e317655cb254bff0cd0ae5cb48fb55732
최윤재 <yunyun3599@gmail.com>
2021-11-05 20:34:19 +0900

    테이블 설계

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    //기본 생성자 필요 (자바 기본 생성자는 public 또는 protected)
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") //order의 member 필드에 의해 변경
+    private List<Order> orders = new ArrayList<>();
+}
